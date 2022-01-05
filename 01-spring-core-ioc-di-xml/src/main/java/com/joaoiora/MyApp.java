1a3cdabc2141df676c1e97bc1d5ccfb70cf1feae
João Iora <joaoiora.42@gmail.com>
2022-01-05 18:07:51 -0300

    Specifying Spring dependencies for baisc Spring Core usage

--- 01-spring-core-ioc-di-xml/src/main/java/com/joaoiora/MyApp.java
+++ 01-spring-core-ioc-di-xml/src/main/java/com/joaoiora/MyApp.java
@@ -4,11 +4,17 @@ import com.joaoiora.model.Coach;
 import com.joaoiora.model.HappyFortuneService;
 import com.joaoiora.model.TrackCoach;
 
+/**
+ * @author João Iora
+ */
 public class MyApp {
 
+  /**
+   * @param args
+   */
   public static void main(String[] args) {
     final Coach coach = new TrackCoach(new HappyFortuneService());
-    System.out.println(coach.getDailyWorkout());
+    System.out.println(coach.getDailyFortune());
   }
 
 }
