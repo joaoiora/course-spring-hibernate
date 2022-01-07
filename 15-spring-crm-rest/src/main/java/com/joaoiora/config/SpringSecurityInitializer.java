bcddd6854eddeb8177130ba300fee96e144dc300
João Iora <joaoiora.42@gmail.com>
2022-01-07 15:32:03 -0300

    add Spring Security authentication & authorization for REST API queries

---
+++ 15-spring-crm-rest/src/main/java/com/joaoiora/config/SpringSecurityInitializer.java
@@ -0,0 +1,11 @@
+package com.joaoiora.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+/**
+ * @author João Iora
+ */
+public class SpringSecurityInitializer
+  extends AbstractSecurityWebApplicationInitializer {
+
+}
